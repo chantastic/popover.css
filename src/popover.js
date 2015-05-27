1e4613ef88dd9377085187c55f64edf3da8642b0
Michael Chan <mijoch@gmail.com>
2015-05-27 09:13:31 -0700

    fix(popover.js): shut eslint up

--- src/popover.js
+++ src/popover.js
@@ -1,8 +1,17 @@
+/*! popover.css v0.0.1 | MIT License | https://github.com/chantastic/popover.css */
+
+/* global document */
+/* global $ */
+
 function getToggledPopoverState(el) {
+  "use strict";
+
   return (el.dataset.hasPopover) ? "" : "open";
 }
 
 function togglePopover(e) {
+  "use strict";
+
   var target = e.target;
 
   if ($(target).closest(".popover").length) {
